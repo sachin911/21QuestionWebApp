014a9a36e9c46c4226ed9bb600d0f8f17b125e2e
sachin <sn1628@nyu.edu>
2019-04-30 00:39:59 -0400

    added the on refresh load call for dashboard

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,12 +3,14 @@ import LoginSaga from '../containers/login/saga'
 import onlineFriends from '../containers/onlineFriends/saga'
 import notification from '../containers/notification/saga'
 import game from '../containers/game/saga'
+import dashboard from '../containers/dashBoard/saga'
 
 export default function* startForman() {
 	yield all([
     fork(LoginSaga),
     fork(onlineFriends),
 		fork(notification),
-		fork(game)
+		fork(game),
+		fork(dashboard)
   ]);
 }
